f4cc64681385903360f7a66c5ca4a2179dbce76d
Lutor Ayangaor <lutor4148@gmail.com>
2022-04-06 07:55:31 +0100

    redesign

---
+++ src/assets/svg/Logo.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
+import * as React from "react";
+import logo from "../logo.png"
+function SvgComponent(props) {
+  return (
+    <>
+    <img src = {logo}></img>
+    </>
+  );
+}
+
+export default SvgComponent;
---
+++ src/screens/ProductList.jsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+import Products from "../components/Sections/Products";
+
+import { useLocation } from "react-router";
+import { useState } from "react";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+  $({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${({ marginRight: "0px" })}
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${({ margin: "10px 0px" })}
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filters] = useState({});
+  const [sort, setSort] = useState("newest");
+
+  // const handleFilters = (e) => {
+  //   const value = e.target.value;
+  //   setFilters({
+  //     ...filters,
+  //     [e.target.name]: value,
+  //   });
+  // };
+
+  return (
+    <Container>
+  
+    
+      <Title>{cat}</Title>
+      <FilterContainer>
+        
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products cat={cat} filters={filters} sort={sort} />
+      </Container>
+  );
+};
+
+export default ProductList;
